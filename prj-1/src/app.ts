a5c1cc6bdc7704a206fa79fda65e34fbac87e772
VanshSh <vanshsharma9354@gmail.com>
2024-03-09 23:47:52 +0530

    feat: :sparkles: Interacting with DOM element

--- prj-1/src/app.ts
+++ prj-1/src/app.ts
@@ -1,3 +1,44 @@
-// Code goes here!
+class ProjectManager {
+  templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
+  element: HTMLFormElement
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
 
-class ProjectManager {}
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement
+    this.element.id = 'user-input'
+    this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    )! as HTMLInputElement
+
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    )! as HTMLInputElement
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    )! as HTMLInputElement
+
+    this.configure()
+    this.attach()
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  }
+  private submitHandler(event: Event) {
+    event.preventDefault()
+    console.log('😇 L-34 in app.ts=> ', this.titleInputElement.value)
+  }
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this))
+  }
+}
+
+const prjInput = new ProjectManager()
