ff6cbcea2a9175e38cf4ebb3138e43c1d16fba75
VanshSh <vanshsharma9354@gmail.com>
2024-03-13 21:47:24 +0530

    feat: :sparkles: Using a getter

--- prj-1/src/app.ts
+++ prj-1/src/app.ts
@@ -147,6 +147,32 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void
 }
 
+// Project Item class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project
+
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person'
+    } else {
+      return `${this.project.people} persons`
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id)
+    this.project = project
+    this.configure()
+    this.renderContent()
+  }
+  configure(): void {}
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title
+    this.element.querySelector('h3')!.textContent = this.persons + 'assigned'
+    this.element.querySelector('p')!.textContent = this.project.description
+  }
+}
+
 // Project List
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[]
@@ -161,9 +187,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.getElementById(`${this.type}-projects-list`)!
     listEl.innerHTML = ''
     for (const projItem of this.assignedProjects) {
-      const listItem = document.createElement('li')
-      listItem.textContent = projItem.title
-      listEl.appendChild(listItem)
+      new ProjectItem(this.element.querySelector('ul')!.id, projItem)
     }
   }
   configure() {
